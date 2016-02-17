f34d65dd3651c888871f106234b5cae155082b30
InnerPeaceStudios <nicolashkpip@gmail.com>
2016-02-17 18:42:58 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-function other(){return"howdy"}function test(){return"Hi there"}
+var ref = new Firebase("https://<realchatbyips>.firebaseio.com");
+ref.authWithOAuthPopup("github", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
