6ff10952e150334bbbb1c3ae8a97c1838849b437
InnerPeaceStudios <nicolashkpip@gmail.com>
2016-03-03 16:26:58 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-var ref = new Firebase("https://realchatbyips.firebaseio.com");
-ref.authWithOAuthPopup("github", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-});
+
